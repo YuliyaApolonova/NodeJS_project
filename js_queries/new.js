5f04f4e6ba8b169a20ae8bcb94f50ef2eafe22bf
YuliyaApolonova <yuliyaapl@mail.ru>
2017-06-13 01:34:03 +0300

    Add new project page

--- js_queries/new.js
+++ js_queries/new.js
@@ -9,7 +9,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/new', function(req, res) {
-   res.render('new_project');
+   if (req.session.username) {
+      res.render('new_project');
+      console.log('Adding is allowed');
+   }
+   else{
+      console.log('Adding in not allowed');
+      res.render('registration');
+   }
 })
 
 module.exports = router;
